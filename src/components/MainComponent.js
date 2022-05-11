6f771a5946587955d90aa5259fa50c47c8bc737b
Erxical <erxinxiao@gmail.com>
2022-05-11 13:47:39 +0700

    Configure Git

---
+++ src/components/MainComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import { DISHES } from '../shared/dishes';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import DishDetail from './DishdetailComponent'
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            selectedDish: null,
+        };
+        this.onDishSelect = this.onDishSelect.bind(this);
+    }
+
+    onDishSelect(dishId) {
+        this.setState({ selectedDish: dishId })
+    }
+
+    render() {
+        const HomePage = () => {
+            return (
+                <Home
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+        }
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} selectedDish={this.state.selectedDish} onClick={(dishId) => this.onDishSelect(dishId)} comments={this.state.comments} />} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default Main;
